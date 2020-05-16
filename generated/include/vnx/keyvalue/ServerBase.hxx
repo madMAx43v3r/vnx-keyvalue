efeb53112d0f9ed70a42f4a6fdc57d4c54b4bcbb
Max Wittal <max.wittal@mwittal.de>
2020-05-16 12:45:45 +0200

    set default num_read_threads to 1

--- generated/include/vnx/keyvalue/ServerBase.hxx
+++ generated/include/vnx/keyvalue/ServerBase.hxx
@@ -26,7 +26,7 @@ public:
 	::vnx::float32_t idle_rewrite_threshold = 0.9;
 	::int32_t rewrite_interval = 10;
 	::int32_t idle_rewrite_interval = 100;
-	::int32_t num_read_threads = 10;
+	::int32_t num_read_threads = 1;
 	::int32_t max_queue_ms = 1000;
 	::vnx::bool_t do_verify_rewrite = 0;
 	::vnx::bool_t ignore_errors = 0;
--- generated/src/ServerBase.cpp
+++ generated/src/ServerBase.cpp
@@ -18,7 +18,7 @@ namespace keyvalue {
 
 
 const vnx::Hash64 ServerBase::VNX_TYPE_HASH(0xbb28aa6f1d808048ull);
-const vnx::Hash64 ServerBase::VNX_CODE_HASH(0x7d7ea1402dfca642ull);
+const vnx::Hash64 ServerBase::VNX_CODE_HASH(0x9a19a17523c976d6ull);
 
 ServerBase::ServerBase(const std::string& _vnx_name)
 	:	Module::Module(_vnx_name)
@@ -194,7 +194,7 @@ std::shared_ptr<vnx::TypeCode> ServerBase::static_create_type_code() {
 	std::shared_ptr<vnx::TypeCode> type_code = std::make_shared<vnx::TypeCode>(true);
 	type_code->name = "vnx.keyvalue.Server";
 	type_code->type_hash = vnx::Hash64(0xbb28aa6f1d808048ull);
-	type_code->code_hash = vnx::Hash64(0x7d7ea1402dfca642ull);
+	type_code->code_hash = vnx::Hash64(0x9a19a17523c976d6ull);
 	type_code->methods.resize(9);
 	{
 		std::shared_ptr<vnx::TypeCode> call_type = std::make_shared<vnx::TypeCode>(true);
@@ -538,7 +538,7 @@ std::shared_ptr<vnx::TypeCode> ServerBase::static_create_type_code() {
 	{
 		vnx::TypeField& field = type_code->fields[9];
 		field.name = "num_read_threads";
-		field.value = vnx::to_string(10);
+		field.value = vnx::to_string(1);
 		field.code = {7};
 	}
 	{
