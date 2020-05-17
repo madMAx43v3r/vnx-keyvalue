fde14d6e0f4c97b8e376bed2029f17b47d1ce248
Max Wittal <max.wittal@mwittal.de>
2020-05-17 23:45:44 +0200

    added vnx_keyvalue_get

---
+++ src/vnx_keyvalue_get.cpp
@@ -0,0 +1,52 @@
+/*
+ * vnx_keyvalue_get.cpp
+ *
+ *  Created on: May 17, 2020
+ *      Author: mad
+ */
+
+#include <vnx/keyvalue/ServerClient.hxx>
+
+#include <vnx/Config.h>
+#include <vnx/Process.h>
+#include <vnx/Terminal.h>
+#include <vnx/Proxy.h>
+#include <vnx/PrettyPrinter.h>
+
+
+int main(int argc, char** argv)
+{
+	std::map<std::string, std::string> options;
+	options["s"] = "server";
+	options["n"] = "name";
+	options["k"] = "key";
+	options["server"] = "server url";
+	options["name"] = "module name";
+	options["key"] = "key";
+	
+	vnx::init("vnx_keyvalue_get", argc, argv, options);
+	
+	std::string server = ".vnx_keyvalue_server.sock";
+	std::string name = "StorageServer";
+	vnx::Variant key;
+	vnx::read_config("server", server);
+	vnx::read_config("name", name);
+	vnx::read_config("key", key);
+	
+	{
+		vnx::Handle<vnx::Proxy> proxy = new vnx::Proxy("Proxy", vnx::Endpoint::from_url(server));
+		proxy->forward_list.push_back(name);
+		proxy.start_detached();
+	}
+	
+	vnx::keyvalue::ServerClient client(name);
+	
+	auto value = client.get_value(key);
+	
+	vnx::PrettyPrinter printer(std::cout);
+	vnx::accept(printer, value);
+	std::cout << std::endl;
+	
+	vnx::close();
+}
+
